5353aaba7a4aa209db6946e60c7d3b058504a7ce
Haroon2402 <aharsha2012@gmail.com>
2024-10-04 09:48:40 +0530

    deleted

--- Simple_Authentication_Project/src/com/sap/Interface/SInterface.java
+++
@@ -1,9 +0,0 @@
-package com.sap.Interface;
-
-import com.sap.Bean.RegisterBean;
-
-public interface SInterface {
-
-	public int register(RegisterBean rb);
-	public int login(String username, String password);
-}
