4867a0e8c7f77a8c132027abd14f7d70a9aef262
Haroon2402 <aharsha2012@gmail.com>
2024-11-08 02:15:20 +0530

    main

--- spotify-admin/src/pages/AddSong.jsx
+++ spotify-admin/src/pages/AddSong.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import axios from 'axios'
 import { url } from '../App'
@@ -48,6 +48,26 @@ const AddSong = () => {
       }
       setLoading(false)
      }
+
+     const loadAlbumData = async () => {
+      try {
+        
+        const response = await axios(`${url}/api/album/list`)
+
+        if(response.data.success){
+          setAlbumData(response.data.albums)
+        }else{
+          toast.error('Unable to load album data')
+        }
+
+      } catch (error) {
+        toast.error("Error Occured")
+      }
+     }
+
+     useEffect(()=>{
+      loadAlbumData()
+     },[])
      
 
   return loading ? (
@@ -89,6 +109,8 @@ const AddSong = () => {
     <p>Album</p>
     <select defaultValue={album} onChange={(e)=>setAlbum(e.target.value)} className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[150px]'>
     <option value="none">None</option>
+    {albumData.map((item,index)=>    <option key={index} value={item.name}>{item.name}</option>
+    )}
     </select>
     </div>
 
